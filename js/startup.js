01d65d379dda440a4abcb1f2c48893e3cb2d8740
Jeffery Antoniuk <jefferya@ualberta.ca>
2014-11-26 16:29:34 -0700

    add new CWRC-Writer startup config

--- js/startup.js
+++ js/startup.js
@@ -39,49 +39,27 @@ function cwrcWriterInit($, Writer, Delegator) {
 
 
   (function() {
-      var configXHR = $.ajax({url: 'http://cwrc-dev-01.srv.ualberta.ca/sites/all/libraries/CWRC-Writer/src/js/writerConfig.js', dataType: 'json'});
-      var projectXHR = $.ajax({url: 'http://apps.testing.cwrc.ca/editor/documents/info/projectname'});
-      $.when(
-          configXHR
-          , projectXHR
-      ).then(function(config) {
-          config = config[0];
-          project = 'editor';
-          config.project = project;
-          moduleUrl = Drupal.settings.basePath;
-          config.cwrcRootUrl = window.location.protocol+'//'+window.location.host+window.location.pathname.replace(window.location.pathname.split('/').pop(),'');
-          doInit(config);
-      },function(xhr) {
-          if (configXHR.state() === 'resolved') {
-              var config = $.parseJSON(configXHR.responseText);
-              //config.cwrcRootUrl = window.location.protocol+'//'+window.location.host+window.location.pathname.replace(window.location.pathname.split('/').pop(),'');
-              //TODO: add path to lib - islandora_markup_editor_add_markup_editor_drupal_settings
-              config.cwrcRootUrl = window.location.protocol + "//" + window.location.hostname + "/sites/all/libraries/CWRC-Writer/src/";
-              //
+      moduleUrl = Drupal.settings.basePath;
 
-              url =  Drupal.settings.basePath + Drupal.settings.islandora_markup_editor.page_setup + PID;
+      url =  Drupal.settings.basePath + Drupal.settings.islandora_markup_editor.page_setup + PID;
 
               $.ajax({
                   url: url, 
                   timeout: 3000,
                   success: function(data, status, xhr) {
                       console.log("Success");
-                      config.project = data;
+                      var config = data;
+                      //TODO: add path to lib - islandora_markup_editor_add_markup_editor_drupal_settings
+                      config.cwrcRootUrl = window.location.protocol + "//" + window.location.hostname + "/sites/all/libraries/CWRC-Writer/src/";
+                      config.project = data; //TODO: is needed by Islandora?
+                      doInit(config);
                       $(window).on('resize', doResize);
                   },
                   error: function() {
                       console.log("failure");
+                      alert('Error loading ' + url);
                   }
               });
-
-
-
-              doInit(config);
-          } else {
-              alert('Error loading writerConfig.js');
-          }
-      });
-
   }());
 };
 
