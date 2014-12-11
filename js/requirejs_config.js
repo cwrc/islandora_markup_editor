68eb51684b260e38826f3258150e13746c73d261
Jeffery Antoniuk <jefferya@ualberta.ca>
2014-12-10 21:08:21 -0700

    changes for new versin of CWRC-Writer

--- js/requirejs_config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-            baseUrl: window.location.protocol + "//" + window.location.hostname + "/sites/all/libraries/CWRC-Writer/src/js",
-            })
--- js/startup.js
+++ js/startup.js
@@ -1,7 +1,33 @@
 var dsid = "OBJ";                                                               
 
+//require.config({baseUrl: 'js'});
+require.config({baseUrl: window.location.protocol + "//" + window.location.hostname + "/sites/all/libraries/CWRC-Writer/src/js"});
+
+  
+// and the 'jquery-private' module, in the
+// jquery-private.js file:
+define('jquery-private', ['jquery'], function ($) {
+  return $.noConflict(true);
+});
+
+require(['jquery', 'knockout'], function($, knockout) {
+  window.ko = knockout; // requirejs shim isn't working for knockout
+  
+  require(['writer',
+            'delegator',
+            'jquery.layout',
+            'jquery.tablayout'
+  ], function(Writer, Delegator) {
+      $(function() {
+          cwrcWriterInit.call(window, $, Writer, Delegator);
+      });
+  });
+});
+
+
 function cwrcWriterInit($, Writer, Delegator) {
 
+  // hide various interface elements
   $('#page_selector').hide();
   $('#header-inner').hide();
   $('#pageChange').hide();
@@ -12,13 +38,17 @@ function cwrcWriterInit($, Writer, Delegator) {
   
   writer = null;
 
+  // Drupal Specific
   console.log(Drupal.settings.islandora_markup_editor.schema_object['schemas']);
   PID = Drupal.settings.islandora_markup_editor.page_pid;
   window.location.hash = '#' + PID;
 
 
+  // initialize CWRC-Writer
   function doInit(config) {
+
     writer = new Writer(config);
+    writer.init('editor');
     writer.event('writerInitialized').subscribe(function(writer) {
     // load modules then do the setup
     require(['modules/entitiesList','modules/relations','modules/selection',
@@ -36,30 +66,31 @@ function cwrcWriterInit($, Writer, Delegator) {
   }
 
 
+  // read configuration and setup CWRC-WRiter
   (function() {
       moduleUrl = Drupal.settings.basePath;
 
       url =  Drupal.settings.basePath + Drupal.settings.islandora_markup_editor.page_setup + PID;
 
-              $.ajax({
-                  url: url, 
-                  timeout: 3000,
-                  success: function(data, status, xhr) {
-                      console.log("Success");
-                      var config = data;
-                      //TODO: add path to lib - islandora_markup_editor_add_markup_editor_drupal_settings
-                      config.cwrcRootUrl = window.location.protocol + "//" + window.location.hostname + "/sites/all/libraries/CWRC-Writer/src/";
-                      config.project = data; //TODO: is needed by Islandora?
-                      config.id = 'editor';
-                      config.delegator = CustomDelegator;
-                      doInit(config);
-                      $(window).on('resize', doResize);
-                  },
-                  error: function() {
-                      console.log("failure");
-                      alert('Error loading ' + url);
-                  }
-              });
+      $.ajax({
+        url: url, 
+        timeout: 3000,
+        success: function(data, status, xhr) {
+            console.log("Success");
+            var config = data;
+            //TODO: add path to lib - islandora_markup_editor_add_markup_editor_drupal_settings
+            config.cwrcRootUrl = window.location.protocol + "//" + window.location.hostname + "/sites/all/libraries/CWRC-Writer/src/";
+            config.project = data; //TODO: is needed by Islandora?
+            config.id = 'editor';
+            config.delegator = CustomDelegator;
+            doInit(config);
+            $(window).on('resize', doResize);
+        },
+        error: function() {
+            console.log("failure");
+            alert('Error loading ' + url);
+        }
+      });
   }());
 };
 
